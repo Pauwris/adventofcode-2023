60fbec99ca48bfd616f5309f4c06e2ef01bccc48
Kristof Pauwels <pauwris@outlook.com>
2023-12-19 21:11:40 +0100

    day09-part2

---
+++ src/day09-part2.ts
@@ -0,0 +1,41 @@
+import { AdventOfCode, mathAdd } from './utils.js'
+
+class PredictNextValue {
+	sum: number = 0;
+	#lines: number[][] = [];
+	constructor(public hist: number[]) {
+		this.#lines.push(hist);
+
+		while (!this.#lastLineAllZero) {
+			const nextLine: number[] = [];
+			for (let i=0, l=this.#lastLine.length; i<l-1; i++) {
+				nextLine.push(this.#lastLine[i+1] - this.#lastLine[i])
+			}
+			this.#lines.push(nextLine)
+		}
+
+		for (let i=this.#lines.length - 1; i>0; i--) {
+			this.sum = (this.#lines[i-1][0] - this.sum)
+		}
+
+		this.#lines = [];
+	}
+	get #lastLineAllZero():boolean {
+		return this.#lastLine.every(n => n === 0)
+	}
+	get #lastLine(): number[] {
+		return this.#lines[this.#lines.length - 1];
+	}
+}
+
+function main() {
+	const aoc = new AdventOfCode("day9", false);
+
+	const histories = aoc.lines.map(line => new PredictNextValue(line.split(" ").map(item => parseInt(item, 10))))
+	histories.forEach(hist => console.log(JSON.stringify(hist)));
+	
+	const sum: number = histories.map(pnv => pnv.sum).reduce(mathAdd, 0);
+	aoc.printSum(sum);
+}
+
+main();
